6698b96643d6f115aa97885d461a3c2877445dc9
Kyle Christensen <kylehgc@gmail.com>
2026-03-03 21:14:18 -0500

    Revert "feat: command schemas and registry (#17)"
    This reverts commit 49694dcab41fcec80485709d3a55c89acf8925dd.

--- src/client/index.ts
+++ src/client/index.ts
@@ -3,16 +3,4 @@
 /**
  * CLI entry point for cypress-cli.
  */
-export {
-	declareCommand,
-	parseCommand,
-	CommandValidationError,
-	type CommandCategory,
-	type CommandDefinition,
-	type CommandSchema,
-	type ParsedCommand,
-	type PositionalMapping,
-	type CommandRegistryEntry,
-} from './command.js';
-
-export { allCommands, buildRegistry, commandRegistry } from './commands.js';
+export {};
